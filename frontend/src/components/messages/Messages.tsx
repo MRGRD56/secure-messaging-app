ff4b564f035ae76b4c5b7ac7d1a88cb2a65d7258
k.golikov <kgolikov@37bytes.com>
2022-11-03 23:53:49 +0500

    Implement user avatars in the messenger

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -4,6 +4,7 @@ import Message from '../message/Message';
 import classNames from 'classnames';
 import {Typography} from '@mui/material';
 import MessageOut from '../../common/types/MessageOut';
+import Gravatar from 'react-gravatar';
 
 interface Props {
     messages: MessageOut[];
@@ -15,9 +16,34 @@ const Messages: FunctionComponent<Props> = ({clientId, messages, className}) => 
     return (
         <div className={classNames(styles.container, className)}>
             {messages.length > 0 ? (
-                messages.map((message, index) => (
-                    <Message key={index} message={message} isMine={clientId === message.clientId}/>
-                ))
+                messages.map((message, index) => {
+                    const nextMessage: MessageOut | undefined = messages[index + 1];
+
+                    const isMyMessage = clientId === message.clientId;
+
+                    const hasNextMessageIndent = nextMessage !== undefined && nextMessage.clientId !== message.clientId;
+                    const isAvatarShown = !isMyMessage && (nextMessage === undefined || nextMessage.clientId !== message.clientId);
+
+                    return (
+                        <div
+                            key={index}
+                            className={classNames(
+                                styles.messageContainer,
+                                isMyMessage && styles.messageContainerMine,
+                                hasNextMessageIndent && styles.messageContainerNextMessageIndent
+                            )}
+                        >
+                            {!isMyMessage && (
+                                <div className={styles.avatarWrapper}>
+                                    {isAvatarShown && (
+                                        <Gravatar email={message.clientId} className={styles.avatar}/>
+                                    )}
+                                </div>
+                            )}
+                            <Message message={message} isMine={isMyMessage}/>
+                        </div>
+                    );
+                })
             ) : (
                 <Typography variant="subtitle2" align="center" className={styles.noMessages}>
                     No messages yet
