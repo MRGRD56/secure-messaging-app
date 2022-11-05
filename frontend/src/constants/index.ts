c7385c75d7035ce7799931f772ac238ccb59d9ec
k.golikov <kgolikov@37bytes.com>
2022-11-05 16:37:34 +0500

    Update api host

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -3,5 +3,5 @@ const {hostname, protocol} = window.location;
 export const API_PORT = process.env['REACT_APP_API_PORT'] ?? '80';
 export const BASE_URI = window.origin;
 export const API_BASE_URI = hostname.endsWith('mrgrd56.ru')
-    ? `${protocol}//api.${hostname}`
+    ? `${protocol}//api-${hostname}`
     : `${protocol}//${hostname}:${API_PORT}`;
